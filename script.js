1e808bac5d773a8536ff02aa0ce3ba336567ce86
vsafonova <viktoriyablak@gmail.com>
2023-02-20 15:55:30 +0100

    Changed the parameters, added new information

--- script.js
+++ script.js
@@ -14,25 +14,23 @@ const projects = [
 ];
 
 const otherProjects = [
-  // {
-  //   name: 'I was poisoned by Putin',
-  //   link: 'https://en.wikipedia.org/wiki/Poisoning_of_Alexei_Navalny#:~:text=On%2020%20August%202020%2C%20Russian,was%20hospitalized%20in%20serious%20condition.',
-  //   description:
-  //     'On 20 August 2020, I was poisoned with a Novichok nerve agent and was hospitalized in serious condition.',
-  //   tags: ['Novichok'],
-  // },
-  // {
-  //   name: 'Now I am in prison',
-  //   link: 'https://en.wikipedia.org/wiki/Poisoning_of_Alexei_Navalny#Return_and_imprisonment',
-  //   description:
-  //     '6/8 In my last word, I spoke, of course, about the war. About how it became a direct consequence of corruption and degradation. A bloody cover for the failure of the Putin regime.',
-  //   tags: ['Stand with Ukraine'],
-  // },
+  {
+    name: 'Psychology',
+    description:
+      'There is no limit to perfection in self-development, since self-development lasts our whole life. My true path to myself began in 2019, when I became interested in psychological knowledge. Since then, I have come to a harmonious relationship with myself and with the people around me.',
+    tags: ['Psychology', 'Self-development', 'Soft-skills', 'Self-awareness', 'Psychotherapy'],
+  },
+  {
+    name: 'Travels',
+    description:
+      'For me, travel is about self-development. I love discovering new places on our amazing planet and getting to know new cultures.',
+    tags: ['Travels', 'Nature', 'Beautiful planet', 'Discovery new places', 'Culture'],
+  },
 ];
 
 const articles = [
   {
-    image: 'img/article.jpg',
+    image: 'img/article1.jpg',
     title: 'The cat-and-mouse experience of Proton, a Swiss company, shows what it"s like to be targeted by Russian censors — and what it takes to fight back.',
     link: 'https://www.nytimes.com/2022/12/06/technology/russia-internet-proton-vpn.html',
     description:
@@ -42,11 +40,7 @@ const articles = [
 
 projects.forEach((item) => {
   projectsContainer.innerHTML += `
-  <a
-  href=${item.link}
-  rel="noopener"
-  target="_blank"
->
+  <a href=${item.link} rel="noopener" target="_blank">
   <div class="card">
     <div class="image-container" style="background-image: url('')">
       <img src="${item.image}"/>
@@ -68,39 +62,21 @@ projects.forEach((item) => {
 
 otherProjects.forEach((item) => {
   otherContainer.innerHTML += `
-  <a
-  href=${item.link}
-  rel="noopener"
-  target="_blank"
->
   <div class="content-container">
     <h2>${item.name}</h2>
-    <p>
-    ${item.description}
-      <span class="arrow-link">>></span>
-    </p>
+    <p> ${item.description}</p>
   </div>
   <div class="other-tags-container">
   ${item.tags.map((tag) => `<span class="tags">${tag}</span>`).join('')}
   </div>
-</a>
   `;
 });
 
 articles.forEach((item) => {
   articlesContainer.innerHTML += `
-  <a
-  class="card"
-  href=${item.link}
-  rel="noopener"
-  target="_blank"
->
+  <a class="card" href=${item.link} rel="noopener" target="_blank">
   <div class="image-container">
-    <img
-      class="image"
-      src=${item.image}
-      alt="code-article"
-    />
+    <img class="image" src=${item.image} alt="code-article"/>
   </div>
   <div class="card-text">
     <h2>${item.title}</h2>
